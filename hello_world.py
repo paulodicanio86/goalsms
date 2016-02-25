186b134f3de395d54151dfa8335be64a3f3f7158
Paul Schaack <paul.schaack2@gmail.com>
2016-02-25 13:53:57 +0000

    hello world example

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+db = []
+
+@app.route('/')
+def start():
+    return 'Hello World!'
+
+
+@app.route('/sms')
+def add_entry():
+    db.append('entry')
+    return('SMS received')
+
+@app.route('/show')
+def show_entries():
+    return(','.join(db))
+
+@app.route('/clear')
+def clear():
+    db = []
+    return('cleared')
+
+if __name__ == '__main__':
+    app.run()
