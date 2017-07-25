ab244bfecba1b4bc436f6965a00bb3ecc0d31694
Paul Schaack <paul.schaack2@gmail.com>
2017-07-25 10:55:42 +0100

    messagebird

--- sms_hunt/sms.py
+++ sms_hunt/sms.py
@@ -13,7 +13,7 @@ keywords = sms_content['keywords']
 start_keyword = sms_content['start_keyword']
 
 
-def send_sms(number, content, config):
+def send_sms_txtlocal(number, content, config):
     encoding = 'utf-8'
     username = config['username']
     sender = config['sender']
@@ -41,6 +41,48 @@ def send_sms(number, content, config):
     except urllib2.URLError, error:
         # print 'Send failed!'
         print error.reason
+
+
+def send_sms_messagebird(number, content, config):
+    encoding = 'auto'
+    sender = config['sender']
+    test_flag = config['test_flag']  # real messages have test_flag = 0
+    key = ''
+
+    if test_flag == 0:
+        key = config['live_key']
+    elif test_flag == 1:
+        key = config['test_key']
+
+    url = config['url_send']
+
+    values = {
+        'access_key': key,
+        'originator': sender,
+        'body': content,
+        'recipients': number,
+        'datacoding': encoding
+    }
+
+    post_data = urllib.urlencode(values)
+    request = urllib2.Request(url)
+
+    # print 'Attempt to send SMS ...'
+    try:
+        f = urllib2.urlopen(request, post_data)
+        fr = f.read()
+        # print 'SMS sent!'
+
+    except urllib2.URLError, error:
+        # print 'Send failed!'
+        print error.reason
+
+
+def send_sms(number, content, config, service='txtlocal'):
+    if service == 'txtlocal':
+        send_sms_txtlocal(number, content, config[service])
+    elif service == 'messagebird':
+        send_sms_messagebird(number, content, config[service])
 
 
 class Sms:
@@ -113,7 +155,7 @@ class Sms:
         # Add message to table
         insert_array_to_table(table_name, db, get_table_columns('tables/' + table_name + '_table.json'), text_row)
 
-    def send(self):
+    def send(self, service='txtlocal'):
         # Function to send a sms
         if self.multiple_receivers:
             receiver = ','.join(self.receiver)
@@ -126,5 +168,5 @@ class Sms:
         else:
             number = self.receiver
 
-        send_sms(number, self.content, sms_config)
+        send_sms(number, self.content, sms_config, service)
         print ('SMS SENT to ' + str(number) + ' and content: ' + self.content)
